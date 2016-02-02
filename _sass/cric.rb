4ff7d9832d828d68201340edf549ca5572636eb9
paras <rockypro92@gmail.com>
2016-02-02 10:41:37 +0530

    Adding portfolio files from Portfolio repo

---
+++ _sass/cric.rb
@@ -0,0 +1,25 @@
+arr = [0,1,2,4,0,3,4,2,1,6,3,0,4,0,2,1,1,2,6];
+sachin = [];
+dravid = [];
+over_count = 0;
+runs= 0;
+
+if (over_count/6 == 0)  
+  over_count++
+  striker_change
+end
+
+def striker_change
+
+  if striker == sachin
+    return dravid
+  else
+    return sachin
+  end
+end
+
+if (runs/2 == 0) 
+  runs.push(sachin)
+else 
+  return striker_change
+end
---
+++ js/project.js
@@ -0,0 +1,36 @@
+function gallery(){
+  $('.p1').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">PSD to HTML</h4><hr><img class="img-responsive all p1" src="images/psd2html1.png"><br/><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/psd2html1" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/psd2html1/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+  $('.p2').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">PSD to HTML</h4><hr><img class="img-responsive" src="images/riding.png"><br/><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/machine-angels-club" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/machine-angels-club/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+  $('.c1').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Calculator</h4><hr><img class="img-responsive" src="images/calci.png"><br/><p class="text-center">A simple Calculator app built using Javascript and jquery</p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/calci" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/calci/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+  $('.g1').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">2048 game</h4><hr><img class="img-responsive" src="images/game.png"><br/><p class="text-center">An image matching game forked it and modified it.... </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/game_2048" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/game_2048/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+  $('.w1').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Jekyll website</h4><hr><img class="img-responsive" src="images/tbbt.png"><br/><hr><p class="text-center">Made a website using jekyll site server </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/tbbt" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/tbbt/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+//   $('#mystartup').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">MIST Blogs</h4><hr><iframe width="560" height="315" src="https://www.youtube.com/embed/YResQaSabUk" frameborder="0" allowfullscreen></iframe><hr><p class="text-center">MIST Blogs is a social Network and microblogging platform for colleges, where each student has a personal website with a unique URL. The Student can design his/her homepage with a built-in drag & drop builder, can join communities, add friends, share files with friends and can also have a remainder of his schedule with the built-in tasks page.</p>'); });
+//   $('#advid').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">AdVid</h4><hr><img class="img-responsive" src="images/proj_advid9.png"><hr><p class="text-center">AdVid is a video advertising platform aimed at increasing brand-awareness among users in a unique interactive way.</p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/ad_vid/tree/develop" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/weather_forecast/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+//   $('#shareit').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">ShareIt</h4><hr><img class="img-responsive" src="images/shareit_project.png"><hr><p class="text-center">A simple Reddit clone. </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/reddit_clone" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://shareit9.herokuapp.com" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+//   $('#inspire').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">98 Inspirations</h4><hr><img class="img-responsive" src="images/project_inspire.png"><hr><p class="text-center">98 Inspirations is your best source of inspiration. User can post inspiring images/quotes, other users can like and comment on them. </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/98inspirations" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ninety-eight-inspirations.herokuapp.com" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+//   $('#angular').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Weather Forecast</h4><hr><img class="img-responsive" src="images/weather_project.png"><hr><p class="text-center">A simple Weather forecast app built using AngularJS.</p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/weather_forecast" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://parasaro92.github.io/weather_forecast/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+//   $('#eureka').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">StartupEureka</h4><hr><img class="img-responsive" src="images/startupeureka.png"><br/><hr><p class="text-center">StartupEureka is a place where you can discover how Startup founders got their Startup ideas, through a 2 min video.</p><div class="col-md-offset-4 col-md-8">&nbsp;&nbsp;&nbsp;<a href="http://startupeureka.com" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+}
+
+$(document).ready(gallery);
+$('document').ready(function() {
+  $('#all').click(function() {
+      $('.all').fadeIn(1000);
+    });
+    $('#html').click(function() {
+      $('.all').fadeOut(1000);
+      $('.p1').fadeIn(1000);
+      $('.p2').fadeIn(1000);
+      $('.g1').fadeIn(1000);
+    });
+    $('#js').click(function() {
+      $('.all').fadeOut(1000);
+      $('.c1').fadeIn(1000);
+      $('.w1').fadeIn(1000);
+    });
+
+    // $('header nav').affix({
+    // offset: {
+    //   top: 80
+    // }
+});
