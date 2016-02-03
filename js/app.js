db1ca68c8c3091ef5ff3cc35eacbac3f6311e94e
paras <rockypro92@gmail.com>
2016-02-03 12:05:40 +0530

    Added animation to sections and final changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
   new WOW().init();
+
+  // $('header nav').affix({
+  //   offset: {
+  //     top: 50
+  //   }
+  // });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
 });
