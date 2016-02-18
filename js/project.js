ad2ce7ddb25987c8d2afa08aa2c9cc00e18b507f
paras <rockypro92@gmail.com>
2016-02-18 14:45:57 +0530

    Added new project portfolio

--- js/project.js
+++ js/project.js
@@ -7,6 +7,7 @@ function gallery(){
   $('.rails').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Todo-App</h4><hr><img class="img-responsive" src="images/todo.png"><br/><hr><p class="text-center">Todo-App in which you can create new tasks, edit tasks and even shift your tasks toongoing to completion. It is a great app to track your progress and even make it easier solves </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/Todo-app" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://cryptic-earth-3671.herokuapp.com/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
   $('.rails1').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Book-store</h4><hr><img class="img-responsive" src="images/book.png"><br/><hr><p class="text-center">This is Bookstore app built using Ruby on Rails.</p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/Book-store" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://murmuring-tor-9444.herokuapp.com/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
   $('.rails2').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">Dostbook</h4><hr><img class="img-responsive" src="images/dost.png"><br/><hr><p class="text-center">This is cool coding social network. In this app, you can send friend request and accept friend requests. you can see what others are doing on your timeline. </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/parasaro92/Dostbook" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://desolate-river-8893.herokuapp.com/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
+  $('.rails3').click(function(ev) { ev.preventDefault(); bootbox.alert('<h4 class="text-center">MedRek</h4><hr><img class="img-responsive" src="images/med1.jpg"><br/><hr><p class="text-center">Patient can track their medical records and upcoming appointments. Doctor can track their patient records and health issues. </p><div class="col-md-offset-3 col-md-9"><a href="https://github.com/CodeAstra/MedRec" target="_blank"><button type="button" class="btn btn-success">Github URL</button></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://medrek.herokuapp.com/" target="_blank"><button type="button" class="btn btn-danger">Launch Demo</button></a></div>'); });
 }
 
 $(document).ready(gallery);
@@ -30,6 +31,7 @@ $('document').ready(function() {
       $('.rails').fadeIn(1000);
       $('.rails1').fadeIn(1000);
       $('.rails2').fadeIn(1000);
+      $('.rails3').fadeIn(1000);
     });
 
     // $('header nav').affix({
